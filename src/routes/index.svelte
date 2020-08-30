56b30d42d8fb4235ce55fd4a72ad4cd1c3cbcb6f
sali0 <saadali93@gmail.com>
2020-08-29 23:08:25 -0500

    Added tailwind and began styling. Broke the text box somehow.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,22 +5,23 @@
   //$: showTranscript = !!transcriptText;
 
   async function getTranscript(videoId_) {
-    const apiUrl = 'api/transcribe';
-    const response = await fetch(`${apiUrl}?v=${videoId_}`)
+    const apiUrl = "api/transcribe";
+    const response = await fetch(`${apiUrl}?v=${videoId_}`);
 
     return response.json();
   }
 
   async function submitVideoURL() {
+    console.log(videoUrl);
     // TODO Test for youtube's share url: https://youtu.be/DORxM-QsUr0
     try {
-      const videoId = new URL(videoUrl).searchParams.get('v')
-      const transcript= await getTranscript(videoId);
+      const videoId = new URL(videoUrl).searchParams.get("v");
+      const transcript = await getTranscript(videoId);
       transcriptText = transcript.text;
-      console.log("transcriptText is " + transcript.text)
-    } catch(err) {
+      console.log("transcriptText is " + transcript.text);
+    } catch (err) {
       // TODO return red error somewhere, maybe flash
-      console.log('Please enter a valid URL')
+      console.log("Please enter a valid URL");
     }
   }
 </script>
@@ -29,21 +30,57 @@
   <title>Hieroglyph</title>
 </svelte:head>
 
-<body class="font-mono bg-gray-400">
-  <div class="container mx-auto">
-    <div>
-      <input bind:value={videoUrl} type="text" placeholder="Insert Youtube Video URL" />
-      <button on:click={submitVideoURL}>Submit</button>
+<div class="flex justify-between text-sm text-grey-700">
+  <div class="flex items-center">
+    <img src="logo_symbol.svg" alt="Home" />
+    <a class="inline-block p-3">Home</a>
+    <a class="inline-block p-3">About</a>
+  </div>
+  <div>
+    <a class="inline-block p-3">Follow Us</a>
+  </div>
+</div>
+
+<div class="max-w-3xl mx-auto pt-20 divide-y-2 divide-gray-600">
+  <div class="pb-4">
+    <img class="mx-auto mb-6" src="logo_banner.svg" alt="Hieroglyph" />
+
+    <div class="border border-gray-200 rounded-md p-2 shadow text-xl relative">
+      <!-- TODO entire text box is not used for some reason. url not getting submitted -->
+      <form on:submit|preventDefault={submitVideoURL}>
+        <input
+          class="outline-none px-3"
+          type="text"
+          placeholder="Insert Youtube Link Here" />
+        <button class="absolute right-0 top-0 mt-4 mr-4">
+          <svg
+            class="h-4 w-4 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            id="Capa_1"
+            x="0px"
+            y="0px"
+            viewBox="0 0 56.966 56.966"
+            style="enable-background:new 0 0 56.966 56.966;"
+            xml:space="preserve"
+            width="512px"
+            height="512px">
+            <path
+              d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23
+              s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92
+              c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z
+              M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17
+              s-17-7.626-17-17S14.61,6,23.984,6z" />
+          </svg>
+        </button>
+      </form>
     </div>
   </div>
-  
-  <!-- Transcript header -->
-  <div>
-    <h5>Transcript</h5>
-  </div>
 
-  <!-- TODO create a transcriptText container component -->
   {#if !!transcriptText}
-  <div>{transcriptText}</div>
+    <p>fjdksaljfkldsa</p>
+    <div>{transcriptText}</div>
   {/if}
-</body>
+
+</div>
