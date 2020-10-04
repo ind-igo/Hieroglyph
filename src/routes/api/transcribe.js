83c59fe941031d149c5c7033c3965f0a42b52e2b
sali0 <saadali93@gmail.com>
2020-10-04 17:41:13 -0500

    Removing Vercel. Moving to Render now that app is containerized.

--- src/routes/api/transcribe.js
+++ src/routes/api/transcribe.js
@@ -3,8 +3,7 @@ import { getTranscript } from '../_services/youtube_transcriber.js';
 
 export async function get(req, res) {
 	const videoId = req.query.v;
+	// TODO Get ok from transcriber and return 202
 	const transcript = await getTranscript(videoId);
-	// Enable Vercel to cache response for 1 year
-	res.set('Cache-Control', 's-maxage=30000000, stale-while-revalidate');
 	res.json({ text: transcript })
 }
